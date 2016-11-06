2caed68ebaa9ad743c81b7bc7db6d8e2241ba41a
Raed Badr <raed0456@hotmail.com>
2016-11-06 01:13:54 -0400

    completed checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
-    
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+     
     var revealPoint = function(index) {
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
@@ -15,3 +15,21 @@ var animatePoints = function() {
     }
   
  };
+
+window.onload = function() {
+    
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+
+    window.addEventListener('scroll', function(event) {
+       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+    });
+}
