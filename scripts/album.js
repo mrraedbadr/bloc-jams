6bd0306d6df4ff3aa0d6af108863ba3cd11dc8ec
Raed Badr <raed0456@hotmail.com>
2016-11-13 23:39:33 -0500

    fixed play button

--- scripts/album.js
+++ scripts/album.js
@@ -31,7 +31,6 @@
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
@@ -60,7 +59,7 @@ var setCurrentAlbum = function(album) {
      }
  };
 
-var songListContainer = document.getElementsByName('album-view-song-list')[0];
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
 
@@ -71,12 +70,13 @@ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></
      
      songListContainer.addEventListener('mouseover', function(event) {
          if (event.target.parentElement.className === 'album-view-song-item') {
-                event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
          }
      });
      
      for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
+             console.log(this);
              this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
          });
      }
